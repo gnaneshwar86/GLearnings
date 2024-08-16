7c5959aa40f324c506b1fb1982a8af5728b62bb9
gnaneshwar86 <gnaneshwarnani8605@gmail.com>
2024-08-16 16:20:03 +0530

    mui files added

--- reactmui/src/App.js
+++ reactmui/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react'
+import {Button,TextField,Container,Typography} from '@mui/material'
 function App() {
+  const [name,setName]=useState('')
+  const handleChange=(e)=>{
+    setName(e.target.value);
+  }
+  const handleSubmit=()=>{
+    alert("Hello "+name)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container maxwidth="sm" style={{marginTop:'50px'}}>
+      <Typography variant="h4" gutterBottom>Welcome All to Material UI</Typography>
+      <TextField label="Enter Your Name : "
+      variant='outlined'
+      fullwidth value={name}
+      onChange={handleChange}
+      style={{marginBottom:'20px'}}/><br></br>
+      <Button variant="contained" color="primary" onClick={handleSubmit}>Submit</Button>
+    </Container>
+  )
 }
-
-export default App;
+export default App
