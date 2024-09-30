a8952afc9d7dc820d8a317d6f689424bfcc38af5
gnaneshwar86 <gnaneshwarnani8605@gmail.com>
2024-09-30 15:05:46 +0530

    MaximumSizeSquareProblem updated

--- DAA/DynamicProgramming/0/MaximumSizeSquare.cpp
+++ DAA/DynamicProgramming/0/MaximumSizeSquare.cpp
@@ -1,19 +1,33 @@
+// You are using GCC
 #include<bits/stdc++.h>
 using namespace std;
-int MSS(vector<vector<int>>& a, int n,int m){
+void MSS(vector<vector<int>>& a, int n,int m){
     vector<vector<int>> dp(n, vector<int>(m));
     int maxel = a[0][0];
+    int k,l;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(i==0 || j==0 || a[i][j]==0)
                 dp[i][j] = a[i][j];
             else 
                 dp[i][j] = min( dp[i-1][j], min( dp[i-1][j-1], dp[i][j-1]) ) + 1;
+            
+            if(dp[i][j] > maxel){
+                maxel = dp[i][j];
+                k=i;
+                l=j;
+            }
         }
-        auto rmax = *max_element(dp[i].begin(), dp[i].end());
-        maxel = max(maxel, rmax);
     }
-    return maxel;
+    //To Print Maximum Size
+    cout<<maxel<<endl
+    //To Print Square Matrix with Maximum Size
+    for(int i=k-maxel+1;i<=k;i++){
+        for(int j=l-maxel+1;j<=l;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 }
 int main(){
     int n,m;
@@ -22,5 +36,5 @@ int main(){
     for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
             cin>>a[i][j];
-    cout<<MSS(a,n,m);
+    MSS(a,n,m);
 }
